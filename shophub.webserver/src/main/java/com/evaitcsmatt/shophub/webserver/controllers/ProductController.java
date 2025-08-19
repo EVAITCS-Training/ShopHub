daa9ede3335432a8914a028882661eb4ec60133b
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-08-19 11:09:47 -0400

    Added Base level Styling

--- shophub.webserver/src/main/java/com/evaitcsmatt/shophub/webserver/controllers/ProductController.java
+++ shophub.webserver/src/main/java/com/evaitcsmatt/shophub/webserver/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.evaitcsmatt.shophub.webserver.controllers;
 
 import java.util.List;
 
+import com.evaitcsmatt.shophub.webserver.dtos.PostNewProduct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,5 +22,11 @@ public class ProductController {
 		List<ProductItem> items = productService.getAllProducts();
 		model.addAttribute("products", items);
 		return "store-index";
+	}
+
+	@GetMapping(value = {"/store/add", "/store/add/"})
+	public String getStoreAddProduct(Model model) {
+		model.addAttribute("product", new PostNewProduct());
+		return "store-add";
 	}
 }
