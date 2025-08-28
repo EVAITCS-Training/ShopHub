033af498dc18adc987029779cb6b2d15cfbc49ec
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-08-28 12:54:44 -0400

    V4 Docker and MVP

--- shophub.webserver/src/main/java/com/evaitcsmatt/shophub/webserver/config/SecurityConfig.java
+++ shophub.webserver/src/main/java/com/evaitcsmatt/shophub/webserver/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.evaitcsmatt.shophub.webserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,10 +38,9 @@ public class SecurityConfig {
                                 .authenticated()
                                 .anyRequest()
                                 .hasRole("ADMIN"))
-
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/store")
-                        .permitAll());
+                .sessionManagement(ses -> ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
